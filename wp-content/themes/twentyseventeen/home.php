ddab9fcdb94c588627fdca48edd04e76c9853653
Jérémy Gérard <jeremy.gerard@sword-group.com>
2018-11-27 14:09:21 +0100

    Add home page template

---
+++ wp-content/themes/twentyseventeen/home.php
@@ -0,0 +1,7 @@
+<?php
+    /*
+     * Template Name: My HTML Homepage
+     */
+?>
+<!-- This page is generated outside of WP by: /wp-content/themes/twentyseventeen/home.php -->
+<!-- Your HTML Code Here -->
