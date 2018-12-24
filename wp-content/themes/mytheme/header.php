7abc5b66eba9e929b0d1d2ded895262120e698d8
Jérémy Gérard <jeremy.gerard@sword-group.com>
2018-12-24 13:06:22 +0100

    Add logo

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -25,7 +25,37 @@
     <?php wp_head();?>
   </head>
   <body>
-      <div class="blog-masthead">
+
+      <header>
+          <nav class="navbar  navbar-fixed-top navbar-default">
+              <div class="container">
+                  <div class="navbar-header">
+                      <button type="button" class="navbar-toggle uarr collapsed" data-toggle="collapse" data-target="#navbar-collapse-uarr">
+                          <span class="sr-only">Toggle navigation</span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                      </button>
+                      <a class="navbar-brand" href="./index.html" title="">
+                          <img src="./images/beehive-search.png" class="navbar-logo-img" alt="">
+                      </a>
+                  </div>
+
+                  <div class="collapse navbar-collapse" id="navbar-collapse-uarr">
+                      <ul class="nav navbar-nav navbar-right">
+                          <li><a href="./index.html" title="" class="active"> Qui sommes-nous </a></li>
+                          <li><a href="./about.html" title=""> Entreprise </a></li>
+                          <li><a href="./pricing.html" title=""> Candidat </a></li>
+                          <li><a href="./contact.html" title=""> Offres d'emploi </a></li>
+                          <li><a href="./components.html" title=""> Contact </a></li>
+                      </ul>
+                  </div>
+              </div>
+          </nav>
+      </header>
+
+
+      <!--div class="blog-masthead">
           <div class="container">
             <nav class="blog-nav">
               <a class="blog-nav-item" href="#">Qui sommes-nous</a>
@@ -35,7 +65,7 @@
               <a class="blog-nav-item" href="#">Contact</a>
             </nav>
           </div>
-      </div>
+      </div-->
 
       <div class="container">
 
