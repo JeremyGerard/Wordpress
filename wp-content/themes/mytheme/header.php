eaf1afc27f9e177511a6cd86bc7d7bcb819e24ba
Jérémy Gérard <jeremy.gerard@sword-group.com>
2018-12-24 13:41:18 +0100

    Logo

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -29,15 +29,19 @@
           <nav class="navbar  navbar-fixed-top navbar-default">
               <div class="container">
                   <div class="navbar-header">
-                      <button type="button" class="navbar-toggle uarr collapsed" data-toggle="collapse" data-target="#navbar-collapse-uarr">
-                          <span class="sr-only">Toggle navigation</span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                      </button>
                       <a class="navbar-brand" href="#index" title="BeeHive">
                           <img src="<?php echo get_bloginfo('template_url') ?>/images/beehive-search.png" class="navbar-logo-img" alt=""/>
                       </a>
+                  </div>
+
+                  <div class="site-header-inner">
+                      <div class="brand header-brand">
+                          <h1 class="m-0">
+                              <a href="#">
+                                  <img src="<?php echo get_bloginfo('template_url') ?>/images/beehive-search.png" alt="Blue logo">
+                              </a>
+                          </h1>
+                      </div>
                   </div>
 
                   <div class="collapse navbar-collapse" id="navbar-collapse-uarr">
