143489e10785953d60cb9bbd3b7da44f083a98f1
Jérémy Gérard <jeremy.gerard@sword-group.com>
2018-12-24 11:46:27 +0100

    Correct sidebar loop

--- wp-content/themes/mytheme/sidebar.php
+++ wp-content/themes/mytheme/sidebar.php
@@ -4,7 +4,7 @@
             <p><?php the_author_meta( 'description' ); ?> </p>
           </div>
           <div class="sidebar-module">
-            <h4>Archives</h4>
+            <h4>Archivessss</h4>
             <ol class="list-unstyled">
             	<?php wp_get_archives( 'type=monthly' ); ?>
             </ol>
