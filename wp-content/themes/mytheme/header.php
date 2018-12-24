94fafaa5b284a85877e61f85cdcccbea95b4d027
Jérémy Gérard <jeremy.gerard@sword-group.com>
2018-12-24 12:45:03 +0100

    update header

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -25,51 +25,21 @@
     <?php wp_head();?>
   </head>
   <body>
-
-  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-      <div class="container">
-          <a class="navbar-brand js-scroll-trigger" href="#page-top">BeeHive</a>
-          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              Menu
-              <i class="fas fa-bars"></i>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-              <ul class="navbar-nav text-uppercase ml-auto">
-                  <li class="nav-item">
-                      <a class="nav-link js-scroll-trigger" href="#services">Qui sommes-nous</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link js-scroll-trigger" href="#portfolio">Entreprise</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link js-scroll-trigger" href="#about">Candidat</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link js-scroll-trigger" href="#team">Offres d'emploi</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
-                  </li>
-              </ul>
+      <div class="blog-masthead">
+          <div class="container">
+            <nav class="blog-nav">
+              <a class="blog-nav-item" href="#">Qui sommes-nous</a>
+              <a class="blog-nav-item" href="#">Entreprise</a>
+              <a class="blog-nav-item" href="#">Candidat</a>
+              <a class="blog-nav-item" href="#">Offres d'emploi</a>
+              <a class="blog-nav-item" href="#">Contact</a>
+            </nav>
           </div>
       </div>
-  </nav>
 
-  <!--div class="blog-masthead">
       <div class="container">
-        <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
-          <a class="blog-nav-item" href="#">New features</a>
-          <a class="blog-nav-item" href="#">Press</a>
-          <a class="blog-nav-item" href="#">New hires</a>
-          <a class="blog-nav-item" href="#">About</a>
-        </nav>
-      </div>
-  </div-->
 
-  <div class="container">
-
-      <div class="blog-header">
-      	<h1 class="blog-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
-      	<p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
-      </div>
+          <div class="blog-header">
+            <h1 class="blog-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
+            <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
+          </div>
