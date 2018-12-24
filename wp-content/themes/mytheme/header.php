d7732c7b8311c50308f52f73d55f36c4a5de4312
Jérémy Gérard <jeremy.gerard@sword-group.com>
2018-12-24 14:13:38 +0100

    fit body

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -45,9 +45,10 @@
               </div>
           </div>
       </nav>
+  </div>
+  <div class="container">
       <h1 class="blog-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
       <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
-  </div>
 
 
       <!--div class="blog-masthead">
