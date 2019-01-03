52c930d4cb311ef3f94ee3f5c704705770e488a6
Jeremy Gerard <jerem.gerard@gmail.com>
2019-01-03 18:23:06 +0100

    Modify search filter

--- wp-content/themes/jobify/resume-filters-flat.php
+++ wp-content/themes/jobify/resume-filters-flat.php
@@ -16,17 +16,6 @@ $atts = apply_filters( 'jobify_resume_filters_flat', array() );
 <form class="resume_search_form resume_search_form--flat" action="<?php echo resume_manager_get_permalink( 'resumes' ) ? resume_manager_get_permalink( 'resumes' ) : get_post_type_archive_link( 'resume' ); ?>" action="GET">
 
 	<div class="search_resumes">
-		<?php do_action( 'resume_manager_resume_filters_search_resumes_start', $atts ); ?>
-
-		<div class="search_keywords resume-filter">
-			<label for="search_keywords"><?php _e( 'Keywords', 'jobify' ); ?></label>
-			<input type="text" name="search_keywords" id="search_keywords" placeholder="<?php _e( 'All Resumes', 'jobify' ); ?>" />
-		</div>
-
-		<div class="search_location resume-filter">
-			<label for="search_location"><?php _e( 'Location', 'jobify' ); ?></label>
-			<input type="text" name="search_location" id="search_location" placeholder="<?php _e( 'Any Location', 'jobify' ); ?>" />
-		</div>
 
 		<?php if ( get_option( 'resume_manager_enable_categories' ) ) : ?>
 
